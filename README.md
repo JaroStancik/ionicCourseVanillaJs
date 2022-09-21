cf6046e656e4037453d8102e3e58ae46b9ecafef
JaroStancik <stancikj@gmail.com>
2022-09-21 11:32:43 +0200

    Init project.

---
+++ README.md
@@ -0,0 +1,1 @@
+# ionicCourseVanillaJs
---
+++ app.js
@@ -0,0 +1,23 @@
+const reasonInput = document.querySelector("#input-reason");
+const reasonAmount = document.querySelector("#input-amount");
+const cancelBtn = document.querySelector("#btn-cancel");
+const confirmBtn = document.querySelector("#btn-confirm");
+const expensesList = document.querySelector("#expenses-list");
+
+confirmBtn?.addEventListener("click", () => {
+  const enteredReason = reasonInput.value;
+  const enteredAmount = reasonAmount.value;
+
+  if (
+    enteredReason.trim().length <= 0 ||
+    enteredAmount <= 0 ||
+    enteredAmount.trim().length <= 0
+  ) {
+    return;
+  }
+  const newItem = document.createElement("ion-item");
+  newItem.textContent = enteredReason + ": $" + enteredAmount;
+
+  expensesList.appendChild(newItem);
+  console.log(enteredReason, reasonAmount.value);
+});
