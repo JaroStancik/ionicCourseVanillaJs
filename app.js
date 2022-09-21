6b0e944ccd2d1f90ab274e64c9defc83efc210e2
JaroStancik <stancikj@gmail.com>
2022-09-21 15:42:39 +0200

    Edited README + total sum.

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const reasonAmount = document.querySelector("#input-amount");
 const cancelBtn = document.querySelector("#btn-cancel");
 const confirmBtn = document.querySelector("#btn-confirm");
 const expensesList = document.querySelector("#expenses-list");
+const totalExpensesOutput = document.querySelector("#total-expenses");
+const alertCtrl = document.querySelector("ion-alert-controller");
+
+let totalExpenses = 0;
+
+const clear = () => {
+  reasonInput.value = "";
+  reasonAmount.value = "";
+};
 
 confirmBtn?.addEventListener("click", () => {
   const enteredReason = reasonInput.value;
@@ -13,6 +22,16 @@ confirmBtn?.addEventListener("click", () => {
     enteredAmount <= 0 ||
     enteredAmount.trim().length <= 0
   ) {
+    alertCtrl
+      .create({
+        // create returns promise, which is fulfilled when the creation is done
+        message: "Please enter valid reason and amount.",
+        header: "Invalid inputs",
+        buttons: ["Okay"],
+      })
+      .then((alertElement) => {
+        alertElement.present();
+      });
     return;
   }
   const newItem = document.createElement("ion-item");
@@ -20,9 +39,10 @@ confirmBtn?.addEventListener("click", () => {
 
   expensesList.appendChild(newItem);
   console.log(enteredReason, reasonAmount.value);
+
+  totalExpenses += +enteredAmount;
+  totalExpensesOutput.textContent = totalExpenses + " € ";
+  clear();
 });
 
-cancelBtn.addEventListener("click", () => {
-  reasonInput.value = "";
-  reasonAmount.value = "";
-});
+cancelBtn.addEventListener("click", clear);
